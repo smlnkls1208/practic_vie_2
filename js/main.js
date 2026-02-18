cf4dd13d738f2c18d2a3fa1c263f3938d5495bd6
smlnkls1208 <al89994950059@gmail.com>
2026-02-18 06:35:00 +0300

    refactor

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+Vue.component('task-component', {
+    props: ['task'],
+    template: `
+        <li>
+            <label>
+                <input type="checkbox" v-model="task.completed">
+                {{ task.text }}
+            </label>
+        </li>
+    `
+})
+
+Vue.component('card-component', {
+    props: ['card', 'columnIndex'],
+    template: `
+        <div class="card">
+            <h3>{{ card.title }}</h3>
+            <ul>
+                <task-component v-for="(task, idx) in card.tasks" :key="idx" :task="task"></task-component>
+            </ul>
+            <button @click="$emit('add-task', columnIndex, card.id)">Добавить пункт</button>
+        </div>
+    `
+})
+
 new Vue({
     el: '#app',
     data() {
