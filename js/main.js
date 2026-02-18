6d81497f13ddbd30152de2be1ccfd07f6aed1c0d
smlnkls1208 <al89994950059@gmail.com>
2026-02-18 05:52:00 +0300

    add task

--- js/main.js
+++ js/main.js
@@ -13,5 +13,16 @@ new Vue({
                 { title: "Завершенные", cards: [] }
             ]
         }
+    },
+    methods: {
+        addTask(columnIndex, cardId) {
+            const text = prompt('Введите задачу')
+            if (!text) return
+
+            const card = this.columns[columnIndex].cards.find(c => c.id === cardId)
+            if (card && card.tasks.length < 5) {
+                card.tasks.push({ text, completed: false })
+            }
+        }
     }
 })
